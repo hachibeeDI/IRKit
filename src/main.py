a686546ab74aed566d4bbe1cd46367ac51ee80fd
OGURA_Daiki <8hachibee125@gmail.com>
2014-08-22 00:10:24 +0900

    cmd interfaceのベース実装

--- src/main.py
+++ src/main.py
@@ -5,12 +5,37 @@ from __future__ import (print_function, division, absolute_import, unicode_liter
 from argparse import ArgumentParser
 
 
-from .irkit.resolve import resolve_irkit_addresses
-from .irkit.info import VERSION
+from irkit.resolve import resolve_irkit_addresses
+from irkit._info import VERSION
+
+
 
 CMD_PARSER = ArgumentParser(description='IRKit CLI Client for Python. v{0}'.format(VERSION))
 
-LOCAL = CMD_PARSER.add_argument_group('local')
-INTERNET = CMD_PARSER.add_argument_group('internet')
 
-base_uri = resolve_irkit_addresses()[0]
+subparsers = CMD_PARSER.add_subparsers(help='sub-command help')
+
+
+def local_func(args):
+    # from irkit import LocalAPI
+    # base_uri = resolve_irkit_addresses()[0]
+    # api = LocalAPI(base_uri)
+    if args.keys:
+        # print(api.keys.post())
+        print('this is your key')
+        return
+    if args.send:
+        print('send ' + args.send)
+
+
+LOCAL_PARSER = subparsers.add_parser('local', help='api for locals.')
+LOCAL_PARSER.add_argument('--keys', action='store_true', help='get a client token.')
+LOCAL_PARSER.add_argument('--send', help='send signals.')
+LOCAL_PARSER.set_defaults(func=local_func)
+
+
+INTERNET = subparsers.add_parser('global', help='api for internets.')
+
+
+args = CMD_PARSER.parse_args()
+args.func(args)
