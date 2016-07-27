8ddb850ef07962e1e6fa0e59ccb4cc4d3f557160
OGURA_Daiki <8hachibee125@gmail.com>
2016-07-27 23:27:49 +0900

    local's commands are works fine

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import (print_function, division, absolute_import, unicode_literals, )
 
-
+import json
 from argparse import ArgumentParser
 
 
@@ -10,7 +10,10 @@ from irkit._info import VERSION
 
 
 
-CMD_PARSER = ArgumentParser(description='IRKit CLI Client for Python. v{0}'.format(VERSION))
+desc = """
+IRKit CLI Client for Python. v{0} See also http://getirkit.com/#IRKit-Device-API
+""".format(VERSION)
+CMD_PARSER = ArgumentParser(description=desc)
 
 
 subparsers = CMD_PARSER.add_subparsers(help='sub-command help')
@@ -22,21 +25,34 @@ def local_func(args):
     if args.host:
         print(base_uri)
         return
+
+
+    api = LocalAPI(base_uri)
     if args.keys:
-        api = LocalAPI(base_uri)
         print(api.keys.post())
         print('this is your key')
         return
     elif args.send:
-        print('send ' + args.send)
+        raw_data = json.loads(args.send)
+        result = api.messages.post(raw_data)
+        print('')
+        print('send signal: ' + unicode(result))
+        return
+    elif args.retrieve:
+        result = api.messages.get()
+        print('')
+        print('retrieve: ' + str(result))
+        return
     else:
         print('need argument. see help')
+        return
 
 
 LOCAL_PARSER = subparsers.add_parser('local', help='api for locals.')
 LOCAL_PARSER.add_argument('--host', action='store_true', help='show irkit host')
 LOCAL_PARSER.add_argument('--keys', action='store_true', help='get a client token.')
-LOCAL_PARSER.add_argument('--send', help='send signals.')
+LOCAL_PARSER.add_argument('--retrieve', action='store_true', help='retrieve a singnal')
+LOCAL_PARSER.add_argument('--send', help='send a signal data or api response')
 LOCAL_PARSER.set_defaults(func=local_func)
 
 
