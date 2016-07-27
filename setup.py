19501402ec6fcef34009c2cf7b8f656769f97bdb
OGURA_Daiki <8hachibee125@gmail.com>
2016-07-27 23:49:35 +0900

    make setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+install_requires = open('requirements.txt').read().split('\n')
+readme_content = open('README.rst').read()
+
+from shutil import copyfile
+copyfile('./main.py', './bin/irkit')
+
+
+setup(
+    name='irkit',
+    version='0.0.1',
+    description='Command line toolkit IRKIt HTTP api',
+    long_description=readme_content,
+    author='OGURA_Daiki',
+    author_email='',
+    license='MIT',
+    keywords=['irkit', 'smart home'],
+    url='https://github.com/hachibeeDI/IRKit',
+    packages=find_packages(),
+    scripts=['bin/irkit'],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
