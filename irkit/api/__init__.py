4e7553277991c61ca844ba7f7499d9476f4973db
OGURA_Daiki <8hachibee125@gmail.com>
2016-08-06 12:24:02 +0900

    tweak for internet api

--- irkit/api/__init__.py
+++ irkit/api/__init__.py
@@ -3,7 +3,7 @@ from __future__ import (print_function, division, absolute_import, unicode_liter
 
 from six import add_metaclass
 
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger
 logger = getLogger(__name__)
 
 import json
@@ -13,6 +13,9 @@ from abc import ABCMeta
 from .local import (
     Messages as LocalMessages,
     Keys as LocalKeys,
+)
+from .internet import (
+    Messages as InternetMessages,
 )
 
 from requests import (
@@ -25,25 +28,49 @@ from requests import (
 @add_metaclass(ABCMeta)
 class BaseAPI(object):
 
-    def __init__(self, host):
-        self.base_uri = 'http://' + host
+    def __init__(self, host, is_https=False):
+        if is_https:
+            self.base_uri = 'https://' + host
+        else:
+            self.base_uri = 'http://' + host
 
     def get(self, resource_uri, parameters=None):
+        logger.debug('url is ' + self.base_uri)
+
         params = parameters or {}
         full_path = path.join(self.base_uri, resource_uri)
-        logger.debug(full_path + ' params = ' + str(parameters))
-        return get(full_path, params=params)
 
-    def post(self, resource_uri, parameters):
+        r = get(full_path, params=params)
+        logger.debug('request to {}: {}'.format(r.request.url, r.text))
+        if r.status_code == 200:
+            result = r.text.decode('utf-8')
+            return json.loads(result or '{}')
+        else:
+            raise ValueError(r.text)
+
+
+    def post(self, resource_uri, raw_payload=None, payload=None):
+        """
+        IRKit basically accept JSON-Encoded data so you should use :raw_payload:.
+        But InternetAPI use form parameter with encoded data so you should use :payload:.
+        """
+        logger.debug('url is ' + self.base_uri)
+
         full_path = path.join(self.base_uri, resource_uri)
-        # IRKit need raw style params
-        raw_params = json.dumps(parameters)
-        logger.debug(full_path + ' params = ' + raw_params)
 
-        r = post(full_path, raw_params)
+        if raw_payload:
+            param = json.dumps(raw_payload)
+        else:
+            param = payload
+
+        logger.debug(full_path + ' params = ' + str(param))
+
+        r = post(full_path, param)
         if r.status_code == 200:
             return json.loads(r.text or '{}')
-        raise ValueError(repr(r))
+        else:
+            logger.error(r.text)
+            raise ValueError(r.text)
 
 
 class API(BaseAPI):
